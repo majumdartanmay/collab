d115299e9a73777483c6ca5dbdc43ac725a8b8e0
majumdartanmay <tanmay.majumdar@hotmail.com>
2024-04-02 23:40:34 +0530

    Fix casing issue of Entry.jsx import in linux

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Entry from "./entry"
+import Entry from "./Entry"
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
